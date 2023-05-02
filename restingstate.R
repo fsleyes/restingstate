39c132cdc01cd385aa5ad4aaded8a78c269f7355
fsleyes <shawn.wang.1667@gmail.com>
2023-05-02 16:41:12 -0700

    simple correlations with difference in target as well as preliminary lm

---
+++ restingstate.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+
+rawData <- read.csv(file = "restingstate_data.csv", header = TRUE)
+
+data <- rawData %>% 
+  filter(!is.na(pre_pair1)) %>%
+  mutate(diff_pair1 = post_pair1 - pre_pair1,
+         diff_pair3 = post_pair3 - pre_pair3,
+         diff_pair4 = post_pair4 - pre_pair4,
+         diff_target = post.target - Pre.target,
+         diff_control = post.control - pre.control)
+
+cor_target_pair1 <- cor(data$diff_target, data$diff_pair1, method = "pearson")
+cor_target_pair3 <- cor(data$diff_target, data$diff_pair3, method = "pearson")
+cor_target_pair4 <- cor(data$diff_target, data$diff_pair4, method = "pearson")
+
+reg_target_pair1 <- lm(diff_target ~ diff_pair1, data = data)
+summary(reg_target_pair1)
+
+
+cor_pair1 <- cor(data$induction_avg, data$diff_pair1, method = "pearson")
+cor_pair3 <- cor(data$induction_avg, data$diff_pair3, method = "pearson")
+cor_pair4 <- cor(data$induction_avg, data$diff_pair4, method = "pearson")
+
+reg_pair1 <- lm(induction_avg ~ diff_pair1, data = data)
+summary(reg_pair1)
