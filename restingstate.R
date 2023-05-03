fa38c5ee9bad79124a4108b92ddfbcd547bbe2e7
fsleyes <shawn.wang.1667@gmail.com>
2023-05-03 12:33:43 -0700

    added to files with other calculations, knitted into html

--- restingstate.R
+++ restingstate.R
@@ -12,14 +12,18 @@ data <- rawData %>%
          diff_control = post.control - pre.control)
 data$pre_date <- lubridate::mdy(data$pre_date)
 data$post_date <- lubridate::mdy(data$post_date)
-data <- data %>% mutate(diff_date = difftime(post_date, pre_date, units = "days"))
+data <- data %>% 
+  mutate(diff_date = difftime(post_date, pre_date, units = "days")) %>%
+  mutate(target_control_pre_raw = Pre.target - pre.control,
+         target_control_post_raw = post.target - post.control,
+         target_control_diff = target_control_post_raw - target_control_pre_raw)
 
 
 cor_target_pair1 <- cor(data$diff_target, data$diff_pair1, method = "pearson")
 cor_target_pair3 <- cor(data$diff_target, data$diff_pair3, method = "pearson")
 cor_target_pair4 <- cor(data$diff_target, data$diff_pair4, method = "pearson")
 
-reg_target_pair1 <- lm(diff_target ~ diff_pair1 + diff_date, data = data)
+reg_target_pair1 <- lm(diff_target ~ diff_pair1 + diff_date + diff_control, data = data)
 summary(reg_target_pair1)
 
 
@@ -29,5 +33,8 @@ summary(reg_target_pair1)
 # 
 # reg_pair1 <- lm(induction_avg ~ diff_pair1, data = data)
 # summary(reg_pair1)
+
+
+
 
 
